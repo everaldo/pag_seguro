f4d940eafb2ea8b997cea093e596d84d2bd6401e
Everaldo Gomes <everaldo.gomes@gmail.com>
2013-06-24 01:23:14 +0000

    removed email and token parameters from Notification initializer. Now, Notification delegates this methods to the Configuration Object

--- lib/pag_seguro/notification.rb
+++ lib/pag_seguro/notification.rb
@@ -1,17 +1,23 @@
 module PagSeguro
   class Notification < Transaction
+    extend Forwardable
 
-    def initialize(email, token, notification_code)
+    def_delegators :config, :email, :token
+
+    def initialize(notification_code)
       raise "Needs a notification code" if notification_code.blank?
       raise "Needs an email" if email.blank?
       raise "Needs a token" if token.blank?
-      @data = transaction_data(email, token, notification_code)
+      @data = transaction_data(notification_code)
     end
 
     private
-      def transaction_data(email, token, notification_code)
+      def transaction_data(notification_code)
         notification_url = "#{PAGSEGURO_TRANSACTIONS_URL}/notifications/#{notification_code}"
         super RestClient.get notification_url, params: {email: email, token: token}
+      end
+      def config
+        PagSeguro.config
       end
   end
 end
